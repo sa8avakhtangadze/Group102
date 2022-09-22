b945c82dcaeaace65edaae7db247191576ca908e
sa8avakhtangadze <sweetnightmare2k22@gmail.com>
2022-09-22 12:58:41 +0400

    second commit

---
+++ DAY1/lesson1.py
@@ -0,0 +1,11 @@
+from unicodedata import name
+
+
+name = "saba"
+surname = "vakhtangadze"
+height = 178.5
+age = 21 
+has_big_plans = True
+is_adult = False
+
+print (name + " " + surname + " " + str(height) + " " + str(age) + " " + str(True) + " " + str(False))
